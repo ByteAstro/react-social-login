746ab288056c6b6b9ac4da6a76e03b74b8d62c08
ByteAstro <biswajitswain348@gmail.com>
2024-05-18 09:01:41 +0530

    project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+### Todos
+- Implement Github login
+- Implement Google login
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='bg-[#151515] h-screen w-full text-white'>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
